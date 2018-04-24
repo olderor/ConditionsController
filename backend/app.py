fecdb858ee21f9b2892f4ec019b181ce7569d57d
Yevchenko Bohdan <98body@gmail.com>
2018-04-24 18:34:39 +0300

    fix cloud9

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,12 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
+    # Cloud9 setup.
+    # app.run(host='0.0.0.0', port=8080, debug=True)
     app.run(port=5002, debug=True, ssl_context=('certificates/cert.pem', 'certificates/key.pem'))
